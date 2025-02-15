bd1bee1c8c7ef5203abbb719e4c91ef8c1be4c7b
SheldonLiu <164149097+SheldonLiu0412@users.noreply.github.com>
2025-02-15 23:52:21 +0800

    去除&符号

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -15,7 +15,6 @@
         </span>
         <!-- 站点备案 -->
         <span>
-          &amp;
           <a v-if="siteIcp" href="https://beian.miit.gov.cn" target="_blank">
             {{ siteIcp }}
           </a>
